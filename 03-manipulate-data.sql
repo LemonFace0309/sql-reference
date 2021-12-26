8b99cdf3ae40098683b15c427327fd8fa3537c26
LemonFace0309 <charles.liu0309@gmail.com>
2021-12-26 01:56:53 -0500

    03-manipulate-data

---
+++ 03-manipulate-data.sql
@@ -0,0 +1,20 @@
+INSERT INTO events (
+  name,
+  date_planned,
+  description,
+  max_participants,
+  min_age
+)
+VALUES (
+  'A first event',
+  '2022-10-29 16:30:00',
+  'A poggers description',
+  20,
+  19
+), (
+  'A second event',
+  '2022-05-29 12:30:00',
+  'Also a poggers description',
+  100,
+  12
+)

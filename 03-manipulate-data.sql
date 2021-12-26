1dedda73e9f8d248999af8d1108b040a96eb48bd
LemonFace0309 <charles.liu0309@gmail.com>
2021-12-26 04:15:22 -0500

    05-create-locations-tables

--- 03-manipulate-data.sql
+++ 03-manipulate-data.sql
@@ -1,20 +1,27 @@
-INSERT INTO events (
-  name,
-  date_planned,
-  description,
-  max_participants,
-  min_age
-)
-VALUES (
-  'A first event',
-  '2022-10-29 16:30:00',
-  'A poggers description',
-  20,
-  19
-), (
-  'A second event',
-  '2022-05-29 12:30:00',
-  'Also a poggers description',
-  100,
-  12
-)
+-- INSERT INTO events (
+--   name,
+--   date_planned,
+--   description,
+--   max_participants,
+--   min_age
+-- )
+-- VALUES (
+--   'A first event',
+--   '2022-10-29 16:30:00',
+--   'A poggers description',
+--   20,
+--   19
+-- ), (
+--   'A second event',
+--   '2022-05-29 12:30:00',
+--   'Also a poggers description',
+--   100,
+--   12
+-- );
+
+UPDATE events
+SET min_age = 16
+WHERE id = 1;
+
+-- DELETE FROM events
+-- WHERE id = 1;
---
+++ 04-query-data.sql
@@ -0,0 +1,3 @@
+SELECT * FROM events
+-- WHERE date_planned >= '2022-06-01' OR min_age = 12;
+ORDER BY id DESC;

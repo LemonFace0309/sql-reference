721259d185cc520cf700291f25baef311a7e9100
LemonFace0309 <charles.liu0309@gmail.com>
2021-12-27 03:20:00 -0500

    10-more-queries

--- 09-queries-with-relations.sql
+++ 09-queries-with-relations.sql
@@ -1,31 +1,31 @@
--- SELECT 
---   e.id AS event_id,
---   e.name,
---   e.date_planned,
---   loc.title,
---   loc.street,
---   loc.house_number,
---   loc.city_name
--- FROM events AS e
--- INNER JOIN locations AS loc ON e.location_id = loc.id;
+SELECT 
+  e.id AS event_id,
+  e.name,
+  e.date_planned,
+  loc.title,
+  loc.street,
+  loc.house_number,
+  loc.city_name
+FROM events AS e
+INNER JOIN locations AS loc ON e.location_id = loc.id;
 
--- SELECT
---   e.id AS event_id,
---   e.name,
---   e.date_planned,
---   loc.title,
---   loc.street,
---   loc.house_number,
---   loc.city_name,
---   u.first_name,
---   u.last_name
--- FROM events AS e
--- INNER JOIN locations AS loc ON e.location_id = loc.id
--- INNER JOIN events_users as eu ON eu.event_id = e.id
--- INNER JOIN users AS u ON eu.user_id = u.id;
+SELECT
+  e.id AS event_id,
+  e.name,
+  e.date_planned,
+  loc.title,
+  loc.street,
+  loc.house_number,
+  loc.city_name,
+  u.first_name,
+  u.last_name
+FROM events AS e
+INNER JOIN locations AS loc ON e.location_id = loc.id
+INNER JOIN events_users as eu ON eu.event_id = e.id
+INNER JOIN users AS u ON eu.user_id = u.id;
 
--- SELECT * FROM locations AS loc
--- LEFT JOIN events AS e ON e.location_id = loc.id;
+SELECT * FROM locations AS loc
+LEFT JOIN events AS e ON e.location_id = loc.id;
 
 SELECT * FROM cities
 LEFT JOIN locations ON locations.city_name = cities.name
---
+++ 10-more-queries.sql
@@ -0,0 +1,22 @@
+-- SELECT * FROM users
+-- WHERE first_name LIKE 'Ma%';
+
+-- SELECT COUNT(id) FROM locations;
+-- SELECT MIN(id) FROM locations;
+-- SELECT MAX(id) FROM locations;
+-- SELECT SUM(id) FROM locations;
+-- SELECT AVG(id) FROM locations;
+
+-- SELECT cities.name, COUNT(locations.id) FROM cities
+-- LEFT JOIN locations ON locations.city_name = cities.name
+-- GROUP BY cities.name;
+
+-- SELECT cities.name, locations.street, COUNT(locations.id) FROM cities
+-- LEFT JOIN locations ON locations.city_name = cities.name
+-- GROUP BY cities.name, locations.street
+-- ORDER BY cities.name;
+
+SELECT cities.name, COUNT(locations.id) FROM cities
+LEFT JOIN locations ON locations.city_name = cities.name
+GROUP BY cities.name
+HAVING COUNT(locations.id) > 1; -- HAVING is for filtering with aggregation functions (like how WHERE is used for raw data)
